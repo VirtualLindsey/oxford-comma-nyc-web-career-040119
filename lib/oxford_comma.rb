dde3db7131638437744d482bdc47d04d64ba51ad
L D <tapircode@gmail.com>
2019-03-12 03:11:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  else if array.length == 2
+    return "#{array[0]} and #{array[1]}"
   result = ""
   for i in 0...(array.length-1)
     result += "#{array[i]}, "
