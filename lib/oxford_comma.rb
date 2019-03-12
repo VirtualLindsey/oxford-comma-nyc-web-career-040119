dd5184e8f7342b9c2c930034d2f36d36888f6093
L D <tapircode@gmail.com>
2019-03-12 03:08:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   result = ""
   for i in 0..array.length-1
-    result += "#{array[result]}, "
+    result += "#{array[i]}, "
   end
   result += "and #{array[array.length]}"
   return result
